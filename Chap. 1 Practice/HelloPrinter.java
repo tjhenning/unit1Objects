3dc4f062c95e507d2a4a6c97b3553cd257cebf33
tjhenning <tjhenning@NN123-HA-09.HS.sd203.org>
2015-08-24 14:12:16 -0500

    Prints Hello

---
+++ Chap. 1 Practice/HelloPrinter.java
@@ -0,0 +1,8 @@
+public class HelloPrinter
+{
+    public static void main(String[] args)
+    {
+        //display a greeting in console window
+        System.out.println( "Hello, World!");
+    }
+}
